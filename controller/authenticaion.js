593c29155245344e896a14c5a78c761134f797f7
PrasadLightMetrics <prasad.raj@lightmetrics.co>
2022-01-31 13:21:29 +0530

    removed customer name

--- controller/authenticaion.js
+++ controller/authenticaion.js
@@ -1,38 +1,39 @@
-
-import authService from '../services/auth';
-import { request200 } from '../request';
-import { errorFirst as eF} from '../utils';
-import { sign } from 'jsonwebtoken';
-
+import authService from "../services/auth";
+import { request200 } from "../request";
+import { errorFirst as eF } from "../utils";
+import { sign } from "jsonwebtoken";
 
 export const verifyAuthentication = async (req, res, next) => {
-    const { code, customerName } = req.body;
-    // STEP1: Exchange auth code for access token
-    const accessTokenOptions = authService.getAccessToken({
-        code,
-        customerName
-    });
-    const [tokenError, tokenDetails] = await eF(request200(accessTokenOptions, req, res));
+  const { code } = req.body;
+  // STEP1: Exchange auth code for access token
+  const accessTokenOptions = authService.getAccessToken({ code });
+  const [tokenError, tokenDetails] = await eF(
+    request200(accessTokenOptions, req, res)
+  );
 
+  // STEP2: Get user details using above access token
+  const { access_token: accessToken, token_type: tokenType } =
+    tokenDetails.body;
+  const userDetailsOptions = authService.getUserDetails({
+    accessToken,
+    tokenType,
+  });
+  const [errorUserDetails, userDetails] = await eF(
+    request200(userDetailsOptions, req, res)
+  );
+  console.log("userDetails", userDetails);
+  // STEP3: Extract response object from userDetails
+  //   const [respObjError, { userMetadata, ...jwtPayload }] = await eF(constructRespObject({userDetails}));
 
-
-    // STEP2: Get user details using above access token
-    const { access_token: accessToken, token_type: tokenType } = tokenDetails.body;
-    const userDetailsOptions = authService.getUserDetails({ accessToken, tokenType });
-    const [errorUserDetails, userDetails] = await eF(request200(userDetailsOptions, req, res));
-
-     // STEP3: Extract response object from userDetails
-//   const [respObjError, { userMetadata, ...jwtPayload }] = await eF(constructRespObject({userDetails}));
-
-   const date = Date.now();
-   const jwt = sign(
+  const date = Date.now();
+  const jwt = sign(
     {
       local: {
         userDetails,
         date,
       },
     },
-    'dgvmtbvuae',
+    "dgvmtbvuae",
     { expiresIn: 86400 }
   );
   return res.status(200).send({
@@ -40,4 +41,4 @@ export const verifyAuthentication = async (req, res, next) => {
     tokenExpiresAt: date + 86400 * 1000,
     token: jwt,
   });
-}
+};
